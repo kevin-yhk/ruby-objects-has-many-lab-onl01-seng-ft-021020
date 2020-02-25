6ae01e0c12009e763d77e877bfcef91ef5fa79ef
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 01:13:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,27 @@
 class Song
   attr_accessor :artist, :name
-
+ 
+  @@all = []
+  
   def initialize(name)
     @name = name
+    save
   end
-
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
   def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
+    
+    if artist 
+    self.artist.name
+   end 
   end
-
+  
+    
 end
