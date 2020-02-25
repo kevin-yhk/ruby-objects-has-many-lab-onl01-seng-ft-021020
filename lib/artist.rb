36928b7e63b3a188ef9a929d020b7ef06c2a9b5c
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 01:12:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,26 @@
 class Artist
   attr_accessor :name
-  @@song_count = 0
+  
   def initialize(name)
     @name = name
-    @songs = [] #empty collection of songs
   end
-
+  
   def songs
-    @songs
+    Song.all
   end
-
+  
   def add_song(song)
-    @songs << song
     song.artist = self
-    @@song_count += 1
   end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
   end
-
+  
   def self.song_count
-    @@song_count
+    Song.all.count
   end
-
+  
+  
 end
