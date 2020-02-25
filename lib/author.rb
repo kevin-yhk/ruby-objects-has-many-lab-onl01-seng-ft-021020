25fee0b7dc82351212723813cbbc21d57514a304
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 01:30:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,4 +12,13 @@ class Author
   def add_post(posts)
     posts.author = self 
   end 
+  
+  def add_post_by_title(title)
+    posts = Post.new(title)
+    add_post(posts)
+  end
+    
+  def self.post_count
+    Post.all.count
+  end
 end 
